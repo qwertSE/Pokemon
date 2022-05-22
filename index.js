b49cc4071e4825b8a39771951cdfa121307e00cb
Dan <danilobomfim.si@gmail.com>
2022-05-21 23:21:06 -0300

    Adicionado novas funcionalidades.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function pokemon() {
+function loadPokemon() {
   let id = new String(document.getElementById("find").value).toLowerCase();
 
   let url = `https://pokeapi.co/api/v2/pokemon/${id}`;
@@ -10,11 +10,13 @@ function pokemon() {
       document.getElementById("pokeinfo").style.display = "block";
 
       document.getElementById("nome").innerHTML = data["name"];
-      document.getElementById("numero").innerHTML = data["id"];
+      document.getElementById("numero").innerHTML = `#${data["id"]}`;
       let pic = data["sprites"]["front_default"];
-      console.log(pic);
-      document.getElementById("foto").setAttribute('src', pic);
+      document.getElementById("foto").setAttribute("src", pic);
       document.getElementById("foto").style.display = "block";
+      let tipo = data.types[0].type.name;
+      document.getElementById("tipo").innerHTML = tipo;
+      document.getElementById("tipo").className = `background-${tipo}`;
     })
     .catch((error) => {
       alert("Verifique se o nome ou o número está correto.");
